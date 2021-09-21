80aba85386070b2a4b5c730afe1864a4df8a0491
Kyle-Wi <black1848@naver.com>
2021-09-21 17:29:25 +0900

    20210921 spring boot jpashop prac ~

---
+++ src/main/java/com/jpabook/jpashop_prac/JpashopPracApplication.java
@@ -0,0 +1,13 @@
+package com.jpabook.jpashop_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopPracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpabook/jpashop_prac/JpashopPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpabook.jpashop_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
